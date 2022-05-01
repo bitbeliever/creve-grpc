e491a89a9ec81472ce0719ea035d6144322ae2a8
shox <shoxhuang@gmail.com>
2022-05-01 16:19:55 +0800

    remove redis 4 test

--- model/redis.go
+++ model/redis.go
@@ -1,19 +1,13 @@
 package model
 
-import (
-	"context"
-	"github.com/go-redis/redis/v8"
-	"log"
-)
-
 func init() {
-	log.Println("redis init")
-	cli := redis.NewClient(&redis.Options{})
-
-	r, err := cli.Ping(context.Background()).Result()
-	if err != nil {
-		panic(err)
-	}
-	log.Println("redis ping", r)
-	println(cli.Set(context.Background(), "hello", "world", 0).Result())
+	//log.Println("redis init")
+	//cli := redis.NewClient(&redis.Options{})
+	//
+	//r, err := cli.Ping(context.Background()).Result()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//log.Println("redis ping", r)
+	//println(cli.Set(context.Background(), "hello", "world", 0).Result())
 }
